ffd7fc38ff4118444b29366a117d01677cd547f9
Queila Souto <monteiro.queila@gmail.com>
2021-10-03 20:44:18 -0300

    inclusão do mapa ao manifesto

--- app/src/main/java/com/example/projetozeradengue/view/fragments/MapsFragment.java
+++ app/src/main/java/com/example/projetozeradengue/view/fragments/MapsFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.projetozeradengue.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
