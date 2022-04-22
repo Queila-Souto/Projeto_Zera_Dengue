daf24c2b501f11d929faa0438cc2a3e1a592fff1
Queila Souto <monteiro.queila@gmail.com>
2022-04-21 21:44:22 -0300

    Conectando o Firebase e setando a opção offline

--- app/src/main/java/com/example/projetozeradengue/view/activity/Splash.java
+++ app/src/main/java/com/example/projetozeradengue/view/activity/Splash.java
@@ -1,7 +1,6 @@
 package com.example.projetozeradengue.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -9,46 +8,34 @@ import android.os.Bundle;
 import android.os.Handler;
 import com.example.projetozeradengue.R;
 import com.example.projetozeradengue.datasource.AppDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Splash extends AppCompatActivity {
 
-int time = 10000;
-
+    int time = 10000;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         new CarregamentoParalelo().execute();
-
-
     }
 
     @SuppressLint("StaticFieldLeak")
-    private class CarregamentoParalelo extends AsyncTask<Void,Void,Void>{
-        /**
-         * Override this method to perform a computation on a background thread. The
-         * specified parameters are the parameters passed to {@link #execute}
-         * by the caller of this task.
-         * <p>
-         * This will normally run on a background thread. But to better
-         * support testing frameworks, it is recommended that this also tolerates
-         * direct execution on the foreground thread, as part of the {@link #execute} call.
-         * <p>
-         * This method can call {@link #publishProgress} to publish updates
-         * on the UI thread.
-         *
-         * @param voids The parameters of the task.
-         * @return A result, defined by the subclass of this task.
-         * @see #onPreExecute()
-         * @see #onPostExecute
-         * @see #publishProgress
-         */
+    private class CarregamentoParalelo extends AsyncTask<Void, Void, Void> {
+
         @Override
         protected Void doInBackground(Void... voids) {
-        new AppDatabase(getApplicationContext());
-
+            new AppDatabase(getApplicationContext());
+            puxarDados();
             return null;
+        }
+
+        private void puxarDados() {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+            mDatabase = FirebaseDatabase.getInstance().getReference();
         }
 
         @Override
@@ -57,13 +44,14 @@ int time = 10000;
             goToActivity();
         }
     }
-    private void goToActivity(){
-    Handler handler = new Handler();
-    handler.postDelayed(() -> {
-        Intent intent = new Intent(Splash.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    },time);
+
+    private void goToActivity() {
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(Splash.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, time);
 
     }
 
